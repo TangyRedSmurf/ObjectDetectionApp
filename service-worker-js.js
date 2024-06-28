83af1daf4219c0cc00d95df1fa1d4de4201bae8a
TangyRedSmurf <65650455+TangyRedSmurf@users.noreply.github.com>
2024-06-28 06:09:56 +0100

    Upload files

---
+++ service-worker-js.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'object-detection-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
+  'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs',
+  'https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
