ead0f96037eb54c6c8c624d7c81afdb831a9e5b0
TangyRedSmurf <65650455+TangyRedSmurf@users.noreply.github.com>
2024-06-28 06:36:13 +0100

    Modified service-worker-js.js

--- service-worker-js.js
+++ service-worker-js.js
@@ -1,24 +1,46 @@
-const CACHE_NAME = 'object-detection-pwa-v1';
+const CACHE_NAME = 'object-detection-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192x192.png',
-  '/icon-512x512.png',
-  'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs',
-  'https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd'
+    '/',
+    '/index.html',
+    '/style.css', // Add your CSS file if you have one
+    '/script.js', // Add your JS file if you have one
+    'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs',
+    'https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
 });
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
